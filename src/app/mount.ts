d0c425a9ab13eb3561528a69d19995109ce19b76
xrnm <justin@iterative.consulting>
2020-05-26 19:15:13 -0500

    Mounts first pass

--- src/app/mount.ts
+++ src/app/mount.ts
@@ -1,7 +1,8 @@
 import {Container} from "./container";
 import {Item} from "./item";
+import {Loadable} from "./loadable";
 
-export class Mount {
+export class Mount implements Loadable{
   name: string;
   morale: number;
   current_hp: number;
@@ -14,6 +15,7 @@ export class Mount {
   alignment: string;
   species: string;
   barding: boolean;
+  saddle: boolean;
   intelligence: number;
   inventory: Item[];
   slung_items: Container[];
@@ -24,7 +26,30 @@ export class Mount {
     this.inventory = init.inventory.map(item => new Item(item));
     this.slung_items = init.slung_items.map(container => new Container(container));
   }
-  bardingText(){
-    return this.barding ? 'Yes' : 'No';
+
+  getDescription(){
+    let description = this.species;
+    if(this.saddle)
+      description = 'Saddeled ' + description;
+    if(this.barding)
+      description += ' with barding';
+    return description
   }
+
+  getSlungItems(){
+    return this.slung_items.filter(si=>!si.deleted);
+  }
+
+  load() {
+    if (this.deleted)
+      return 0;
+
+    return this.inventory.reduce((weight, item, index) => {
+      return weight + item.totalWeight()
+    }, this.getSlungItems().reduce((weight,item,index)=>{
+      return weight + item.load();
+    },0));
+  }
+
+
 }
--- src/app/show-character/character-mounts/character-mounts.component.ts
+++ src/app/show-character/character-mounts/character-mounts.component.ts
@@ -3,6 +3,9 @@ import {Character} from "../../character";
 import {GameService} from "../../game.service";
 import {DefaultSettingsHelper} from "../../default-settings-helper";
 import {Mount} from "../../mount";
+import {Container} from "../../container";
+import {ContainerDialogComponent} from "../character-inventory/container-dialog/container-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'character-mounts',
@@ -12,7 +15,7 @@ import {Mount} from "../../mount";
 export class CharacterMountsComponent implements OnInit {
   @Input() character: Character;
   editing: number = -1;
-  constructor(private gameservice: GameService) { }
+  constructor(public dialog: MatDialog, private gameService: GameService) { }
 
   ngOnInit(): void {
   }
@@ -37,4 +40,28 @@ export class CharacterMountsComponent implements OnInit {
       this.editing = -1;
     }
   }
+  openContainerDialog(container: Container = null, mount: Mount){
+    if(this.gameService.lock)
+      return;
+
+    let slung_items = null;
+    if(!container){
+      slung_items = mount.slung_items;
+      container = new Container({name: 'New Container', contents: [], slots: 10, weight: 0})
+    }
+
+    this.dialog.open(ContainerDialogComponent,{
+      data: {
+        container: container,
+        slung_items: slung_items
+      }
+    });
+  }
+
+  deleteSlungItem(slung_item){
+    if(this.gameService.lock)
+      return;
+    slung_item.delete();
+  }
+
 }
