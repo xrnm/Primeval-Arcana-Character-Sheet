f803c044b7fd3fd0b156ec3f27814ca0611a5dc0
Justin Edwards <justin@iterative.consulting>
2022-04-03 20:26:12 -0500

    updating versions and documentation

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -10,7 +10,7 @@ import {NEW_GAME} from "./new_game";
 export class GameService {
   game: Game;
   lock: boolean = false;
-  version = '1.103'
+  version = '1.104'
 
   getGame(): Game {
     return this.game;
