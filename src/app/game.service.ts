fb6c8ae7d85eca9f801106511b21b3fd32dc778d
Justin Edwards <jedwards@elevatodigtial.com>
2026-06-28 21:08:05 -0600

    name change

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -10,7 +10,7 @@ import {NEW_GAME} from "./new_game";
 export class GameService {
   game: Game;
   lock: boolean = false;
-  version = '1.301'
+  version = '1.302'
   getGame(): Game {
     return this.game;
   }
