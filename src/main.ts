388c859801c54f5679ce8db90ebccae1f2058973
Justin Edwards <jedwards@elevatodigtial.com>
2026-08-02 10:57:37 -0600

    Upgrade Angular and Material from 20 to 21

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {enableProdMode, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 
 import {environment} from './environments/environment';
 import {MatNativeDateModule} from '@angular/material/core';
@@ -13,6 +13,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
+        provideZoneChangeDetection(),
         importProvidersFrom(BrowserModule, AppRoutingModule, MatNativeDateModule),
         Title,
         provideAnimations()
